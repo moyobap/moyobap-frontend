d70544085b44af1cc5f33b29ed3805ef2376be11
seungin <tmddls91@inha.edu>
2025-08-29 09:12:56 +0900

    [feat] 타입 정의 관련 index.ts 구성

---
+++ moyobab/src/types/index.ts
@@ -0,0 +1,3 @@
+export type ButtonVariant = "primary" | "secondary" | "ghost" | "danger";
+export type ButtonSize = "sm" | "md" | "lg";
+export type AlertType = "info" | "success" | "warning" | "error";
