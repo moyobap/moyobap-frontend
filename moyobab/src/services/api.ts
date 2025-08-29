5fa90985720722cb60a4c3f1c4237eb5a2a3e09d
seungin <tmddls91@inha.edu>
2025-08-29 09:49:06 +0900

    [test] mock 연동을 위한 임시 API 추가

---
+++ moyobab/src/services/api.ts
@@ -0,0 +1,9 @@
+import type { Group } from "../types";
+import { mockGroups } from "../mocks/groups";
+
+export const api = {
+  getActiveGroups: async (): Promise<Group[]> => {
+    await new Promise((res) => setTimeout(res, 500));
+    return mockGroups;
+  },
+};
