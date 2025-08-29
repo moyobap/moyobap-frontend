1b5ab7b0bd16879474fa93e51031306d679f214f
seungin <tmddls91@inha.edu>
2025-08-29 10:32:11 +0900

    [refactor] 라우팅 config에 NotFound 페이지 추가

--- moyobab/src/router/config.tsx
+++ moyobab/src/router/config.tsx
@@ -2,11 +2,13 @@ import type { RouteObject } from "react-router-dom";
 import HomePage from "../pages/home/page";
 import MatchPage from "../pages/match/page";
 import GroupDetailPage from "../pages/groups/[id]/page";
+import NotFoundPage from "../pages/NotFound";
 
 const routes: RouteObject[] = [
   { path: "/", element: <HomePage /> },
   { path: "/match", element: <MatchPage /> },
   { path: "/groups/:id", element: <GroupDetailPage /> },
+  { path: "*", element: <NotFoundPage /> },
 ];
 
 export default routes;
